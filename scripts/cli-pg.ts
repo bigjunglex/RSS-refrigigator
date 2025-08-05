4510bf4028bc4b245c27795175758060242bb18f
Jungle Smashfist <gearl00taz@gmail.com>
2025-08-05 23:25:50 +0300

    improved script dev start

---
+++ scripts/cli-pg.ts
@@ -0,0 +1,18 @@
+
+/**
+ * script to connect to postgress through cli for debugging
+ * WAS USED IN PREVIUOS ITERATION WITH PostGRES
+ */
+
+import { readConfig } from '../src/config'
+import { spawn } from 'node:child_process'
+
+const cmd = `psql ${readConfig().dbUrl}`;
+
+// console.log(cmd)
+const db = spawn(cmd, {shell: true, stdio: [0, 0, 0]})
+
+process.on('SIGINT', () => { db.kill() })
+process.on('SIGTERM', () => { db.kill() })
+process.on('SIGTSTP', () => { db.kill() })
+
---
+++ scripts/serve.ts
@@ -0,0 +1,36 @@
+import { ChildProcess, spawn } from "child_process";
+
+
+const events:NodeJS.Signals[] = [
+    'SIGTERM',
+    'SIGINT',
+    'SIGQUIT'
+]
+
+const commands:[string, string[]][] = [
+    ['npm', ['run', 'dev-server']],
+    ['npm', ['run', 'dev-client']],
+]
+
+const killSlaves = (slaves:ChildProcess[]) => slaves.forEach(s => s.kill())
+const spawnSlave = (cmd:string, args:string[]):ChildProcess => {
+    const msg = '👹: '
+    console.log(msg, cmd, args)
+    const slave = spawn(cmd, args.flat(), { shell:true })
+    slave.stdout.on('data', (data) => process.stdout.write(data))
+    slave.stderr.on('data', (data) => process.stderr.write(data))
+    slave.on('error', (error) => console.log(msg, cmd, args, error))
+    slave.on('exit', (code) => {
+        if (code !== 0) {
+            console.error(msg, cmd, args, 'exit with', code)
+        }
+        console.log(msg, cmd, args, 'shut down')
+    })
+
+    return slave
+}
+
+const slaves = commands.map(([cmd, args]) => spawnSlave(cmd, args))
+for (const event of events) {
+    process.on(event, () => killSlaves(slaves))
+}
