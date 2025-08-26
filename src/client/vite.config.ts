f4a9d742700d991384b8679e48c728a9435b45bf
Jungle Smashfist <gearl00taz@gmail.com>
2025-08-26 19:57:12 +0300

    swap for preact for lower bundele through viteconfig alias

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import preact from '@preact/preset-vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [preact()],
+  resolve: {
+    alias: {
+      'react': 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
+      'react/jsx-runtime': 'preact/jsx-runtime'
+    }
+  },
+  build:{
+    outDir: "../../dist-client/"
+  }
 })
