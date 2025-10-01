1160c8d51dade1be4f257774727e9f8d8848fa4c
Jungle Smashfist <gearl00taz@gmail.com>
2025-10-01 22:51:33 +0300

    +supertes +setup changes +boilerplate for integration test as error tests

---
+++ src/tests/server/error.test.ts
@@ -0,0 +1,35 @@
+import request from 'supertest';
+import { describe, it, vitest, beforeAll, afterAll } from 'vitest';
+import { setupEnv } from '../utils';
+import createApp from 'src/server';
+
+process.loadEnvFile()
+const OGenv = process.env;
+const app = createApp();
+
+
+beforeAll(() => {
+    vitest.resetModules()
+    const testEnv = setupEnv('./.env.test') 
+    process.env = {...OGenv, ...testEnv}
+})
+
+afterAll(() => {
+    process.env = OGenv
+})
+
+describe('base errors', () => {
+
+    it('gets 500 on /api/check', () => {
+        return request(app)
+            .get('/api/check')
+            .expect(500)
+    });
+
+    it('get 500 on unauthorized favorites', () => {
+        return request(app)
+            .get('/api/feeds/posts/favorites')
+            .expect(500)
+    })
+    
+})
